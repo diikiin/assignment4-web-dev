b1559eedc1951137127f906b509d5844bfa6ff72
Dauren <qabyl.da2@altynbank.kz>
2024-11-29 16:33:03 +0500

    updated permissions

--- blog/permissions.py
+++ blog/permissions.py
@@ -5,4 +5,6 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+        if request.method == "POST":
+            return request.user and request.user.is_authenticated()
         return obj.user == request.user
